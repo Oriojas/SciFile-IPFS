79762a66ecb719fe99c08c9df48f97a351bf7f6d
oriojas <oscarriojas@gmail.com>
2024-03-07 22:04:39 -0500

    feat/ create upload file ipfs

--- ipfs.py
+++ ipfs.py
@@ -1,12 +1,28 @@
 import os
 from lighthouseweb3 import Lighthouse
 
-lh = Lighthouse(token=os.environ['KEY'])
 
-tagged_source_file_path = "pdf_data/"
-file = 'test.pdf'
-tag = "pdf_test"
-upload_with_tag = lh.upload(source=f'{tagged_source_file_path}{file}', tag=tag)
-print("File Upload with Tag Successful!")
+class Ipfs:
 
-print(upload_with_tag.get('data'))
+    def __init__(self):
+        self.lh = Lighthouse(token=os.environ['KEY'])
+
+    def load_pdf(self, file, tag="pdf_test", tagged_source_file_path="pdf_data/"):
+        """
+        this function load ipfs file in lighthouse
+        :param file: file name to load
+        :param tag: tag for identify file class
+        :param tagged_source_file_path: local folder
+        :return:
+        """
+        upload_with_tag = self.lh.upload(source=f'{tagged_source_file_path}{file}', tag=tag)
+        metadata = upload_with_tag.get('data')
+        print("File Upload with Tag Successful!")
+        print(metadata)
+
+        return metadata
+
+
+if __name__ == "__main__":
+    Ipfs().load_pdf(file='test.pdf')
+
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-import os
-import lighthouseweb3
+import uvicorn
+from ipfs import Ipfs
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 
-print('Hello')
+app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
+
+
+@app.post('/load_data_ipfs/')
+def load_data_ipfs(local_file: str):
+    response = Ipfs().load_pdf(file=local_file)
+    response = dict(response)
+
+    json_response: object = jsonable_encoder(response)
+
+    return json_response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8090)
