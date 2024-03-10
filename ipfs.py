445fbbbd4ef17b53bcb26ae43fc1f1921f1b32e6
oriojas <oscarriojas@gmail.com>
2024-03-09 19:11:51 -0500

    feat/ include review endpoint

--- ipfs.py
+++ ipfs.py
@@ -1,4 +1,5 @@
 import os
+import json
 from lighthouseweb3 import Lighthouse
 
 
@@ -7,22 +8,62 @@ class Ipfs:
     def __init__(self):
         self.lh = Lighthouse(token=os.environ['KEY'])
 
-    def load_pdf(self, file, tag="pdf_test", tagged_source_file_path="pdf_data/"):
+    def load_pdf(self, file: str, description: str, tag="pdf_test", file_path="pdf_data/"):
         """
         this function load ipfs file in lighthouse
+        :param description: description article
         :param file: file name to load
         :param tag: tag for identify file class
-        :param tagged_source_file_path: local folder
+        :param file_path: local folder
         :return:
         """
-        upload_with_tag = self.lh.upload(source=f'{tagged_source_file_path}{file}', tag=tag)
+        upload_with_tag = self.lh.upload(source=f'{file_path}{file}', tag=tag)
         metadata = upload_with_tag.get('data')
         print("File Upload with Tag Successful!")
         print(metadata)
 
-        return metadata
+        # {'Name': 'test.pdf', 'Hash': 'QmaEmcgUBVSKtVHbtNZRr18dWK1vWsPSzEiUNTpkAiNe7Z', 'Size': '9028'}
+
+        nft_metadata = {"description": description,
+                        "hash": metadata.get('Hash'),
+                        "name": metadata.get('Name'),
+                        "review": "Initial review",
+                        "n_rev": 0}
+
+        with open(f'{os.environ["ROOT_FOLDER"]}/json_data/nft_metadata.json', 'w') as jf:
+            json.dump(nft_metadata, jf)
+
+        upload_with_tag = self.lh.upload(source=f'json_data/nft_metadata.json', tag=tag)
+        nft_metadata = upload_with_tag.get('data')
+
+        print("File Upload with Tag Successful!")
+        print(nft_metadata)
+
+        return nft_metadata
+
+    def load_rev(self, description: str, hash: str, name: str, review: str, n_rev: int):
+
+        json_data_review = {"description": description,
+                            "hash": hash,
+                            "name": name,
+                            "review": review,
+                            "n_rev": n_rev}
+
+        with open(f'{os.environ["ROOT_FOLDER"]}/json_data/review_metadata.json', 'w') as jf:
+            json.dump(json_data_review, jf)
+
+        upload_dict_tag = self.lh.upload(source='json_data/review_metadata.json')
+        json_metadata = upload_dict_tag.get('data')
+        print("File Upload with Tag Successful!")
+        print(json_metadata)
+
+        return json_metadata
 
 
 if __name__ == "__main__":
-    Ipfs().load_pdf(file='test.pdf')
-
+    Ipfs().load_pdf(file='test.pdf', description='Test file')
+    # Ipfs().load_rev(description="Test file",
+    #                 hash="QmaEmcgUBVSKtVHbtNZRr18dWK1vWsPSzEiUNTpkAiNe7Z",
+    #                 name="test.pdf",
+    #                 review="Test large review XDXDXDXDXDXDXD",
+    #                 n_rev=1)
--- main.py
+++ main.py
@@ -16,13 +16,28 @@ app.add_middleware(CORSMiddleware,
 
 
 @app.post('/load_data_ipfs/')
-def load_data_ipfs(local_file: str):
-    response = Ipfs().load_pdf(file=local_file)
+def load_data_ipfs(local_file: str, description: str):
+    response = Ipfs().load_pdf(file=local_file, description=description)
     response = dict(response)
 
     json_response: object = jsonable_encoder(response)
 
     return json_response
+
+
+@app.post('/load_review/')
+def load_review(description: str, hash: str, name: str, review: str, n_rev: int):
+    response_rev = Ipfs().load_rev(description=description,
+                                   hash=hash,
+                                   name=name,
+                                   review=review,
+                                   n_rev=n_rev)
+
+    response_rev = dict(response_rev)
+
+    json_response_rev: object = jsonable_encoder(response_rev)
+
+    return json_response_rev
 
 
 if __name__ == "__main__":
